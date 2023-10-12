bd59e08e12a2c64c4de59f58889d445532357f4d
PrOwOtogen <sampeters1407@outlook.com>
2023-10-12 20:31:55 +0200

    init

---
+++ characters/player.py
@@ -0,0 +1,13 @@
+class Player():
+    def __init__(self, *args):
+        self.weight = 0
+        self.height = 0
+        self.lust = 0
+        self.clean = 100
+        self.name = "Test"
+        self.age = 0
+        self.sex = ""
+        self.money = 0
+        self.full = 0
+
+myp = Player()
---
+++ main.py
@@ -0,0 +1,12 @@
+import rich
+import time
+import sys
+
+from characters.player import *
+
+def mainmenu():
+    print(myp.name)
+
+
+if __name__ == "__main__":
+    mainmenu()
